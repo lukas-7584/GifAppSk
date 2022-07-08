a7eda70eb9e2f7ef6ed08ef0141edb8f8bfa8877
lucas <amaya.jorgelucas@gmail.com>
2022-07-07 23:13:44 -0300

    Terminando  App Gif

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { getGif } from '../helpers/getGif';
+import { GifItem } from './GifItem';
 
 export const GifGrid = ({category}) => {
-  return (
+
+  const [imagenes, setImagenes] = useState ( [ ] );
+
+  const getImages = async () =>{
+    const newImages =await getGif ( category)
+    setImagenes(newImages);
+  }
+
+useEffect(() =>{
+
+  getImages()
+  
+},  [ ] )
+
+
+  return (  
     <>
 
-       <h3>{category}</h3>
-        
+        <h3>{category}</h3>
+
+          <div className="card-grid">
+            {
+              imagenes.map(imagen => <GifItem key={imagen.id} {...imagen}/> )
+            }
+          </div>
         </>
   )
 }
---
+++ src/helpers/getGif.js
@@ -0,0 +1,22 @@
+    export const getGif = async (category) => {
+    
+        const API_KEY = "yWIdPJtwjfdHOjorbB14jy8dpIHp8V3c";
+        
+        const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${category}&limit=${10}&offset=0&rating=g&lang=en`;
+
+        const resp = await fetch(url);
+
+        const { data } = await resp.json();
+
+        const gifs = data.map(img => (
+            {
+                id: img.id,
+                title: img.title,
+                url: img.images.original.url
+            }
+        ))
+        
+
+    console.log(gifs);
+    return gifs;
+    };
